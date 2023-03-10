c5d945319df76d7e69eb7944ba13aced21f30e66
Muhammad Hammad <mhnaeem.dev@gmail.com>
2023-03-10 11:31:18 -0500

    Add cleanup scripts for the frontend project

---
+++ FrontEnd/scripts/cleanup.bat
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rmdir obj 2> nul
+rmdir bin 2> nul
+rmdir .parcel-cache 2> nul
+rmdir dist 2> nul
---
+++ FrontEnd/scripts/cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf obj
+rm -rf bin
+rm -rf .parcel-cache
+rm -rf dist
